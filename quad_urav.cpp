be96464d20ff8ee6f59069b228325db22945e1fa
pupach <58131107+pupach@users.noreply.github.com>
2023-08-18 17:12:47 +0500

    Add files via upload

--- quad_urav.cpp
+++ quad_urav.cpp
@@ -1,10 +1,14 @@
 #include <TXLib.h>  // И все, и все, больше ниче не надо, траляляляля!!!
 #include <stdio.h>
 #include <math.h>
+#include<windows.h>
+#include<stdlib.h>
 #define degree 2
+static double ep = 1e-8;
 struct quad gen_struktur(double a[]);
 int get_koaf(double a[], int size_a);
 int find_korny_quad(struct quad *urav);
+int print_if_about_korn(struct quad urav);
 struct quad{
     double koaf[degree+2] = {0};
 
@@ -21,21 +25,25 @@ int get_koaf(double a[], int size_a){
 
 
     int len_a = size_a/sizeof(a[0]);
+    printf("Введите пожалуйста %d коэфицентов ",len_a );
     while(i<len_a)   {
         j = scanf("%lf", &k);
         if (j>0){
             a[i] = k;
+            i++;
             }
         else{
-            printf("error");
-            break;
+            printf("Коэфиценты неверный ");
+            fflush(stdin);
             }
-        i++;
     }
+    printf("Коэфиценты введены");
 
 }
 
+
 int find_korny_quad(struct quad *urav){
+    printf("Начинаем поиск корней ");
     double a = (*urav).koaf[0];
     double b = (*urav).koaf[1];
     double c = (*urav).koaf[2] - (*urav).koaf[3];
@@ -44,15 +52,36 @@ int find_korny_quad(struct quad *urav){
 
     (*urav).dis = b*b - 4*a*c;
      dis = (*urav).dis;
-    if (dis >= 0){
+    if (dis>(-ep)){
         (*urav).korn[0] = (-b + sqrt(dis))/(double) 2/a;
         (*urav).korn[1] = (-b - sqrt(dis))/(double) 2/a;
+        printf("Корни найдены ");
+
         return 1;
         }
     else{
-        return EOF;
+        return -1;
     }
+    printf("Корни найдены ");
 }
+
+
+int get_and_print_inf_about_korn(struct quad urav){
+    if (urav.dis<ep){
+        printf("корней нет");
+        return 1;
+    }
+    else if(abs(urav.korn[0] - urav.korn[1]) < ep){
+        printf("%lf - корeнь", urav.korn[0]);
+        return 2;
+        }
+    else{
+        printf("%lf, %lf - корни", urav.korn[0], urav.korn[1]);
+        return 0;
+        }
+}
+
+
 struct quad gen_struktur(double a[degree+2])
 {
     struct quad equ;
@@ -61,22 +90,4 @@ struct quad gen_struktur(double a[degree+2])
     }
     return equ;
 }
-int main()
-{
-    double a[4]={
-    1,2,3, 4
-    };
-    int h = 0;
-    struct quad urav;
 
-
-    h = get_koaf(a, sizeof(a));
-    urav = gen_struktur(a);
-    find_korny_quad(&urav);
-    if (urav.dis >= 0){
-        printf("%lf, %lf - корни", urav.korn[0], urav.korn[1]);
-        }
-    else{
-        printf("корней нет");
-        }
-}
---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <TXLib.h>  // » все, и все, больше ниче не надо, трал€л€л€л€!!!
+#include <stdio.h>
+#include <math.h>
+#include "quad_urav.cpp"
+
+int main()
+{
+    double a[4]={
+    1,2,3, 4
+    };
+    int h = 0;
+    struct quad urav;
+
+
+    h = get_koaf(a, sizeof(a));
+    urav = gen_struktur(a);
+    find_korny_quad(&urav);
+    get_and_print_inf_about_korn(urav);
+}
